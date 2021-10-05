3b80663867e61153153e7f9a438580e2df65692b
Víctor M. Reyes <victor@devf.mx>
2021-10-04 20:00:28 -0600

    feature: run server

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('😍 ');
+const express = require('express');
+const mongoose = require('mongoose');
+// const bodyparser = require('body-parser');
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// capturar body
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Conexión a Base de datos
+
+// import routes
+
+// route middlewares
+app.get('/', (req, res) => {
+  res.json({
+    serverStatus: "ok"
+  })
+});
+
+// iniciar server
+app.listen(PORT, () => {
+  console.log(`Servidor en puerto: ${PORT}`)
+})
