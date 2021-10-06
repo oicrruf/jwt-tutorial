8cb236616f9073e1fdda47d9f1f514210a916df2
Víctor M. Reyes <victor@devf.mx>
2021-10-05 18:45:09 -0600

    feature: validation user data

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Joi = require('@hapi/joi');
+
+const schemaRegister = Joi.object({
+  name: Joi.string().min(3).max(255).required(),
+  email: Joi.string().min(6).max(255).required().email(),
+  password: Joi.string().min(8).max(1024).required()
+})
 
 router.post('/register', async (req, res) => {
+
+  const { error } = schemaRegister.validate(req.body)
+
+  if (error) {
+    return res.status(400).json(
+      { error: error.details[0].message }
+    )
+  }
 
   const user = new User({
     name: req.body.name,
