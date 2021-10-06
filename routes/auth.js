f3adae746dc83ee593de81e24773098a56a49e22
Víctor M. Reyes <victor@devf.mx>
2021-10-05 19:42:07 -0600

    feature: login

--- routes/auth.js
+++ routes/auth.js
@@ -52,4 +52,30 @@ router.post('/register', async (req, res) => {
   }
 })
 
+const schemaLogin = Joi.object({
+  email: Joi.string().min(6).max(255).required().email(),
+  password: Joi.string().min(8).max(1024).required()
+})
+
+router.post('/login', async (req, res) => {
+  // validaciones
+  const { error } = schemaLogin.validate(req.body);
+
+  if (error) return res.status(400).json({ error: error.details[0].message })
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).json({ error: 'Usuario no encontrado' });
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+  if (!validPassword) return res.status(400).json({ error: 'contraseña no válida' })
+
+  res.json({
+    error: null,
+    data: 'exito bienvenido'
+  })
+})
+
+
 module.exports = router;
